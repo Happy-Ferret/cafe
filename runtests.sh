61d669bf0f35f3ac83eebd50078ccf7f433989b8
hydraz <urn@semi.works>
2016-03-27 19:14:42 -0300

    [tests] improvement to runtests.sh

--- runtests.sh
+++ runtests.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/bash
 
 for testsuite in $(grep test package.json | sed -re 's|\s*"(\S+)":.*$|\1|g' | grep test); do
-	echo -e "\x1b[1;34m::\x1b[0m Testing with Lua $(echo $testsuite | sed -re 's/test(.*)$/\1/')"
-	if npm run "$testsuite" | grep "passed"; then
-		echo -e "\x1b[1;32m→\x1b[0m Test $testsuite passed!"
+	echo -e "\x1b[1;34m::\x1b[0m Testing with $(lua$(echo $testsuite | sed -re 's/test(.*)$/\1/') -e 'print(_VERSION);') (lua$(echo $testsuite | sed -re 's/test(.*)$/\1/'))"
+	if which lua$(echo $testsuite | sed -re 's/test(.*)$/\1/') >/dev/null; then
+		if npm run "$testsuite" | grep "status code 0" &>/dev/null; then
+			echo -e "  \x1b[1;32m→\x1b[0m Test $testsuite passed!"
+		else
+			echo -e "  \x1b[1;31m→\x1b[0m Test $testsuite failed!"
+			exit
+		fi
 	else
-		echo -e "\x1b[1;31m→\x1b[0m Test $testsuite failed!"
-		exit
+		echo -e "  \x1b[1;32m→\x1b[0m No interpreter for $testsuite, skipping.."
 	fi
 done
 
