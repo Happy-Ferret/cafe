b1efc0b5494e00869b8c0e29887122891fd65af0
hydraz <urn@semi.works>
2016-03-31 16:47:58 -0300

    [tests] test for an interpreter before heading

--- runtests.sh
+++ runtests.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 for testsuite in $(grep test package.json | sed -re 's|\s*"(\S+)":.*$|\1|g' | grep test); do
-	echo -e "\x1b[1;34m::\x1b[0m Testing with $(lua$(echo $testsuite | sed -re 's/test(.*)$/\1/') -e 'print(_VERSION);') (lua$(echo $testsuite | sed -re 's/test(.*)$/\1/'))"
-	if which lua$(echo $testsuite | sed -re 's/test(.*)$/\1/') >/dev/null; then
+	if which lua$(echo $testsuite | sed -re 's/test(.*)$/\1/') &>/dev/null; then
+		echo -e "\x1b[1;34m::\x1b[0m Testing with $(lua$(echo $testsuite | sed -re 's/test(.*)$/\1/') -e 'print(_VERSION);') (lua$(echo $testsuite | sed -re 's/test(.*)$/\1/'))"
 		if npm run "$testsuite" | grep "status code 0" &>/dev/null; then
 			echo -e "  \x1b[1;32m→\x1b[0m Test $testsuite passed!"
 			rm -rf "$(echo "$testsuite" | sed -re 's/test/&s/;s/\.//g').lua"
@@ -11,7 +11,7 @@ for testsuite in $(grep test package.json | sed -re 's|\s*"(\S+)":.*$|\1|g' | gr
 			exit
 		fi
 	else
-		echo -e "  \x1b[1;32m→\x1b[0m No interpreter for $testsuite, skipping.."
+		echo -e "\x1b[1;32m→\x1b[0m No interpreter for $testsuite, skipping.."
 	fi
 done
 
